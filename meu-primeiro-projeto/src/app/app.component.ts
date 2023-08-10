0197b2bbe9a1f43bb11defc6c9c97fe54133b058
monegasco <mhoa.2000@gmail.com>
2023-08-10 20:37:34 -0300

    aprendendo dataBinding, interpolation

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,23 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-title *ngIf="destruir"></app-title>
-  <br>
-  <button (click)="destruirComponente()">Change Title</button>
+  <app-data-binding></app-data-binding>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
-  public destruir:boolean = true;
   constructor() { }
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('1');
-    }, 5000);
-    
-  }
-  public destruirComponente(){
-    this.destruir = false;
-  }
-  
+  ngOnInit(): void { }
 }
 
