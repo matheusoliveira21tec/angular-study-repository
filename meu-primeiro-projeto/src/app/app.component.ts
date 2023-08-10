969dbaad0718eebacef329e048d8ca71f77c1565
monegasco <mhoa.2000@gmail.com>
2023-08-10 19:28:31 -0300

    aprendendo ciclos de vida, ngOnChange

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+  <app-title title="Welcome to Angular"></app-title>
+  <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
   constructor() { }
--- meu-primeiro-projeto/src/app/title/title.component.ts
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
-  public title: string = 'Welcome to Angular!';
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = 'Welcome to Angular!';
   constructor() { }
   ngOnInit(): void {
+  }
+  ngOnChanges(): void {
+    alert('title changed');
   }
 
 }
