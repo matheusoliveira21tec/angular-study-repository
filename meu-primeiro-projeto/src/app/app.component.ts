911d5077a5befc7b6042fb308cd11c235365d2db
monegasco <mhoa.2000@gmail.com>
2023-08-12 14:04:54 -0300

    aprendendo modules

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <app-new-component></app-new-component>
   <app-diretivas-atributos></app-diretivas-atributos>
   <router-outlet></router-outlet>`
 })
